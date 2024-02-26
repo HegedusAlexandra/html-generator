e6e7b92a5341903014563ef67eb6c33f45a463e7
HegedusAlexandra <alexandra.hegedus@vamosoft.hu>
2024-02-26 11:47:49 +0100

    Added description

--- README.old.md
+++ README.old.md
@@ -1,1 +1,4 @@
 # html-generator
+# shittyclippy component added
+# on the redux branch analready working redux setup dont forget npm i
+
--- src/App.js
+++ src/App.js
@@ -8,19 +8,19 @@ function App() {
   const [output, setOutput] = useState("");
   const [html, setHtml] = useState([]);
   const [object, setObject] = useState([]);
-  const [title,setTitle] = useState("")
+  const [title, setTitle] = useState("");
 
   const handleInputChange = (e) => {
     setInput(e.target.value);
   };
 
-  const art = object
+  const art = object;
 
   const applyStyle = (styleType) => {
     let styledOutput;
     let htmlOutput = [...html];
-    let objectOutput = [ ...object ]; 
-    
+    let objectOutput = [...object];
+
     switch (styleType) {
       case "title":
         styledOutput = (
@@ -29,7 +29,7 @@ function App() {
             <br />
           </>
         );
-        setTitle(input)
+        setTitle(input);
         break;
       case "headline":
         styledOutput = (
@@ -122,25 +122,39 @@ function App() {
     setInput("");
   };
 
-  const renderContent = (el,index) => {
+  const renderContent = (el, index) => {
     switch (el.type) {
       case "headline":
         return (
           <div key={index}>
-            <h2  className='w-[100%] text-[8vw] md:text-[2.5vw] mb-[4vh] mt-[8vh] font-playfair text-text'>{el.content}</h2>
+            <h2 className="w-[100%] text-[8vw] md:text-[2.5vw] mb-[4vh] mt-[8vh] font-playfair text-text">
+              {el.content}
+            </h2>
           </div>
         );
       case "sec_headline":
-        return <h2 key={index} className='md:text-[2vw] text-[6vw] my-[2vh] font-afacad text-text'>{el.content}</h2>;
+        return (
+          <h2
+            key={index}
+            className="md:text-[2vw] text-[6vw] my-[2vh] font-afacad text-text"
+          >
+            {el.content}
+          </h2>
+        );
       case "text":
         return <span key={index}>{el.content}</span>;
       case "picture":
-        return <div key={index} className={`w-[100%] h-[20vh] my-[2vh] rounded-sm ${el.content} bg-no-repeat bg-cover bg-center`}  />;
+        return (
+          <div
+            key={index}
+            className={`w-[100%] h-[20vh] my-[2vh] rounded-sm ${el.content} bg-no-repeat bg-cover bg-center`}
+          />
+        );
       case "list":
         return (
-          <ul key={index} className='py-[2vh]'>
+          <ul key={index} className="py-[2vh]">
             {el.headlines.map((headline, idx) => (
-              <li key={headline+idx}>
+              <li key={headline + idx}>
                 <strong>{headline}</strong>: {el.contents[idx]}
               </li>
             ))}
@@ -149,78 +163,86 @@ function App() {
       default:
         return null;
     }
-  }
+  };
 
   return (
     <div className="app">
-      <Clippy/>
+      <Clippy />
       <div className="fixed z-1 bottom-0 w-[100%]">
-      <div className="w-[100%] flex flex-row">
-        <button
-          className="h-[4vh] py-2 px-4 bg-slate-100 rounded-md m-2 uppercase font-bold shadow-[2px_2px_1px_1px_rgba(0,0,0,1)]"
-          onClick={() => applyStyle("title")}
-        >
-          Title
-        </button>
-        <button
-          className="h-[4vh] py-2 px-4 bg-slate-100 rounded-md m-2 uppercase font-bold shadow-[2px_2px_1px_1px_rgba(0,0,0,1)]"
-          onClick={() => applyStyle("headline")}
-        >
-          Headline
-        </button>
-        <button
-          className="h-[4vh] py-2 px-4 bg-slate-100 rounded-md m-2 uppercase font-bold shadow-[2px_2px_1px_1px_rgba(0,0,0,1)]"
-          onClick={() => applyStyle("sec_headline")}
-        >
-          Secondary
-        </button>
-        <button
-          className="h-[4vh] py-2 px-4 bg-slate-100 rounded-md m-2 uppercase font-bold shadow-[2px_2px_1px_1px_rgba(0,0,0,1)]"
-          onClick={() => applyStyle("text")}
-        >
-          Text
-        </button>
-        <button
-          className="h-[4vh] py-2 px-4 bg-slate-100 rounded-md m-2 uppercase font-bold shadow-[2px_2px_1px_1px_rgba(0,0,0,1)]"
-          onClick={() => applyStyle("picture")}
-        >
-          Picture
-        </button>
-        <button
-          className="h-[4vh] py-2 px-4 bg-slate-100 rounded-md m-2 uppercase font-bold shadow-[2px_2px_1px_1px_rgba(0,0,0,1)]"
-          onClick={() => applyStyle("list")}
-        >
-          List
-        </button>
-        <CopyToClipboard text={html} onCopy={() => alert("Copied HTML!")}>
-        <button className="h-[4vh] py-2 px-4 bg-lime-500 rounded-md m-2 uppercase font-bold shadow-[2px_2px_1px_1px_rgba(0,0,0,1)]">
-          Copy HTML
-        </button>
-      </CopyToClipboard>
-      <CopyToClipboard text={JSON.stringify({[title]: object}, null, 2)} onCopy={() =>{ alert("Copied Object!");console.log(object);}}>
-        <button className="h-[4vh] py-2 px-4 bg-lime-500  rounded-md m-2 uppercase font-bold shadow-[2px_2px_1px_1px_rgba(0,0,0,1)]">
-          Copy Object
-        </button>
-      </CopyToClipboard>
+        <div className="w-[100%] flex flex-row">
+          <button
+            className="h-[4vh] py-2 px-4 bg-slate-100 rounded-md m-2 uppercase font-bold shadow-[2px_2px_1px_1px_rgba(0,0,0,1)]"
+            onClick={() => applyStyle("title")}
+          >
+            Title
+          </button>
+          <button
+            className="h-[4vh] py-2 px-4 bg-slate-100 rounded-md m-2 uppercase font-bold shadow-[2px_2px_1px_1px_rgba(0,0,0,1)]"
+            onClick={() => applyStyle("headline")}
+          >
+            Headline
+          </button>
+          <button
+            className="h-[4vh] py-2 px-4 bg-slate-100 rounded-md m-2 uppercase font-bold shadow-[2px_2px_1px_1px_rgba(0,0,0,1)]"
+            onClick={() => applyStyle("sec_headline")}
+          >
+            Secondary
+          </button>
+          <button
+            className="h-[4vh] py-2 px-4 bg-slate-100 rounded-md m-2 uppercase font-bold shadow-[2px_2px_1px_1px_rgba(0,0,0,1)]"
+            onClick={() => applyStyle("text")}
+          >
+            Text
+          </button>
+          <button
+            className="h-[4vh] py-2 px-4 bg-slate-100 rounded-md m-2 uppercase font-bold shadow-[2px_2px_1px_1px_rgba(0,0,0,1)]"
+            onClick={() => applyStyle("picture")}
+          >
+            Picture
+          </button>
+          <button
+            className="h-[4vh] py-2 px-4 bg-slate-100 rounded-md m-2 uppercase font-bold shadow-[2px_2px_1px_1px_rgba(0,0,0,1)]"
+            onClick={() => applyStyle("list")}
+          >
+            List
+          </button>
+          <CopyToClipboard text={html} onCopy={() => alert("Copied HTML!")}>
+            <button className="h-[4vh] py-2 px-4 bg-lime-500 rounded-md m-2 uppercase font-bold shadow-[2px_2px_1px_1px_rgba(0,0,0,1)]">
+              Copy HTML
+            </button>
+          </CopyToClipboard>
+          <CopyToClipboard
+            text={JSON.stringify({ [title]: object }, null, 2)}
+            onCopy={() => {
+              alert("Copied Object!");
+              console.log(object);
+            }}
+          >
+            <button className="h-[4vh] py-2 px-4 bg-lime-500  rounded-md m-2 uppercase font-bold shadow-[2px_2px_1px_1px_rgba(0,0,0,1)]">
+              Copy Object
+            </button>
+          </CopyToClipboard>
+        </div>
+        <textarea
+          placeholder="your text comes here"
+          className="w-[100%] h-[10vh] bg-slate-300 overflow-y-auto uppercase p-2 onfocus:border-none text-slate-700 font-bold font-montserrat "
+          type="text"
+          value={input}
+          onChange={handleInputChange}
+        />
       </div>
-      <textarea
-        placeholder="your text comes here"
-        className="w-[100%] h-[10vh] bg-slate-300 overflow-y-auto uppercase p-2 onfocus:border-none text-slate-700 font-bold font-montserrat "
-        type="text"
-        value={input}
-        onChange={handleInputChange}
-      />
-      </div>
-      <div className='flex flex-col w-[100%] lg:px-[30%] p-[7vw] py-[20vh] bg-slate-100 overflow-x-hidden'>
+      <div className="flex flex-col w-[100%] lg:px-[30%] p-[7vw] py-[20vh] bg-slate-100 overflow-x-hidden">
         <header>
-          <h1 className='w-[100%] text-headline lg:text-[4vw] text-[10vw] font-playfair font-bold'>{title || "title not yet set"}</h1>
+          <h1 className="w-[100%] text-headline lg:text-[4vw] text-[10vw] font-playfair font-bold">
+            {title || "title not yet set"}
+          </h1>
           <div className="w-[100%] h-[1px] bg-slate-300 rounded" />
-          <div className="flex flex-row justify-between items-center w-[100%] h-[2vh] text-[0.5vw] border-0.5 rounded font-montserrat">
-          </div>
+          <div className="flex flex-row justify-between items-center w-[100%] h-[2vh] text-[0.5vw] border-0.5 rounded font-montserrat"></div>
           <hr className="w-[100%] h-[1px] bg-slate-300 rounded" />
-        </header>        
+        </header>
         {art.map(renderContent)}
       </div>
+      <div className="text-slate-300 text-[3vh] font-bold uppercase p-2">Start to type and hit the required button to add to the site, you will see how it will looks like.</div>
     </div>
   );
 }
